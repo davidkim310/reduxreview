e0a78d78ca905c892f9f4ddd3a6b2c1b1cf77e6f
davidkim310 <davidkim310@gmail.com>
2016-10-16 19:31:47 -0700

    form posts

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-# ReduxSimpleStarter
-
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
-
-###Getting Started###
-
-There are two methods for getting started with this repo.
+# React router practice
 
 ####Familiar with Git?#####
 Checkout this repo, install dependencies, then start the gulp process with the following:
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    const handleSubmit = this.props.handleSubmit;
+    const title = this.props.fields.title;
+    const categories = this.props.fields.categories;
+    const content = this.props.fields.content
+
     return (
-      <form>
+      <form onSubmit={ handleSubmit(this.props.createPost) }>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type='text' className="form-control" />
+          <input type='text' className="form-control" {...title} />
         </div>
 
         <div className="form-group">
-          <label>categories</label>
-          <input type='text' className="form-control" />
+          <label>Categories</label>
+          <input type='text' className="form-control" {...categories}/>
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content}/>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -26,10 +32,11 @@ class PostsNew extends Component {
     );
   }
 }
-
+//connect: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
+//reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
 export default reduxForm({
   form: 'PostsNewForm',
   fields: ['title', 'categories','content']
-})(PostsNew)
+}, null, { createPost })(PostsNew)
 
 //user types something in.... record it on app state
